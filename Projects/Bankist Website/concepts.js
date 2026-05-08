b180751f03a6ccab4ffe41fc960459e4fc221d66
Kratik777 <kratikbhatt6@gmail.com>
2026-05-08 21:39:15 +0530

    Learned ObserverAPI and more concepts

--- Projects/Bankist Website/concepts.js
+++ Projects/Bankist Website/concepts.js
@@ -15,7 +15,7 @@ console.log(allButtons);
 
 console.log(document.getElementsByClassName("btn"));
 
-//THIS IS HOW WE CAN CREATE ELEMENTS ::::::::::::::::::::::::::::::::::::::::
+//THIS IS HOW WE CAN CREATE ELEMENTS ::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 //.insertAdjacentHTML
 
@@ -28,13 +28,13 @@ message.innerHTML = `We use cookies for improving funcionality and analytics.<bu
 header.append(message); // append means last child
 // header.append(message.cloneNode(true)); // if passed false then it will create shallow copy
 
-//THIS IS HOW WE DELETE ELEMENTS ::::::::::::::::::::::::::::::::::::::::::::::
+//THIS IS HOW WE DELETE ELEMENTS :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 const closeBtn = document.querySelector(".btn--close-cookie");
 closeBtn.addEventListener("click", function (e) {
   message.remove();
 });
 
-//STYLES :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//STYLES :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 message.style.backgroundColor = "#37383d";
 message.style.width = "105%";
 
@@ -50,7 +50,7 @@ message.style.height =
 // manipulating the document root
 document.documentElement.style.setProperty("--color-primary", "orangered");
 
-// ATTRIBRUTES :::::::::::::::::::::::::::::::::::::::::::::::::::::
+// ATTRIBRUTES ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 const logo = document.querySelector(".nav__logo ");
 console.log(logo);
 
@@ -72,8 +72,128 @@ console.log(logo.src); // absolute src value
 //Data attributes
 console.log(logo.dataset.versionNumber);
 
-//CLASSES :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//CLASSES ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 // logo.classList.add();
 // logo.classList.remove();
 // logo.classList.toggle();
 // logo.classList.contains();
+
+// GETTING COORDINATES OF ANY ELEMENTS :::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+/*
+
+🔷Yes — getBoundingClientRect() is the main method used to get an element’s coordinates and size relative to the viewport (visible browser window).
+ 
+top → distance from top of viewport
+left → distance from left of viewport
+right → distance from left side to element’s right edge
+bottom → distance from top side to element’s bottom edge
+
+🔷 To get the coordinated of current scroll position i.e. from top of the page to current viewport top. we use scrollX for horizontal scroll and scrollY for vertical scroll 
+
+🔷 We can also get width and height of viewport by using 
+
+document.DocumentElement.clientHeight
+document.DocumentElement.clientWidth methods as well
+
+🔷 To implement the scroll functionality we can use scrollTo() method which is old way of doing this and also we now can use scrollIntoView() method on element where we want scroll to.
+
+
+*/
+
+//Best way of handlind event
+const h1 = document.querySelector("h1");
+
+const h1Handler = function (e) {
+  alert("h1:great! You are reading the heading!");
+};
+
+h1.addEventListener("mouseenter", h1Handler);
+
+setTimeout(() => h1.removeEventListener("mouseenter", h1Handler), 3000);
+
+// old way of setting event handler:
+// h1.onmouseenter = function (e) {
+//   alert(
+//     "different way of attaching event listener by setting on property on html element",
+//   );
+// };
+
+// 🔷 Concept : Event Propogation Bubbling and Capturing
+
+//randomInt
+
+const randomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("LINK", e.target, e.currentTarget);
+
+  //we can stop propagation
+  // e.stopPropagation();
+});
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("LINKS CONTAINER", e.target, e.currentTarget);
+});
+document.querySelector(".nav").addEventListener(
+  "click",
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log("NAV", e.target, e.currentTarget);
+  },
+  true,
+);
+
+// DOM TRAVERSING ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+//going downwards of h1
+
+console.log(h1.querySelectorAll(".highlight")); // all elem with classes highlight in h1. will go deep inside no matter what!
+console.log(h1.childNodes); // they return all types of nodes in a nodelist okay that is text or comment as well
+console.log(h1.children); // this will return only element nodes but inside the html collection also only direct children element only okay
+
+h1.firstElementChild.style.color = `white`;
+h1.lastElementChild.style.color = "teal";
+
+//going upwards of h1
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = `var(--gradient-secondary)`;
+
+//going sideways
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+//both of above method gives only element siblins if not then null
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+// both of the above method give any sibling like text and comment as well
+
+// Intersection observer API - concept
+
+const section1 = document.querySelector("#section--1");
+
+const observerCallback = function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry);
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2],
+};
+
+const observer = new IntersectionObserver(observerCallback, obsOptions);
+observer.observe(section1);
--- Projects/Bankist Website/script.js
+++ Projects/Bankist Website/script.js
@@ -1,9 +1,191 @@
 "use strict";
 
 const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section-1");
+const section1 = document.querySelector("#section--1");
 
 btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+
+  //Using scrollTo method to implement scroll:
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY,
+  // );
+
+  //Smooth scroll implementation:
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: "smooth",
+  // });
+
+  //Modern clean way:
+  section1.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// BAD WAY OF ATTACHING EVENT HANDLER
+// document.querySelectorAll(".nav__link").forEach((elem) => {
+//   elem.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     console.log("LINK");
+//     const id = this.getAttribute("href");
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+//EVENT DELEGATION : Add event listener to common parent element and in that parent listener detect what element originated the event !
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// Building Tabbed Component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clickedBtn = e.target.closest(".operations__tab");
+  if (!clickedBtn) return;
+  tabs.forEach((tab) => {
+    tab.classList.remove("operations__tab--active");
+  });
+  clickedBtn.classList.add("operations__tab--active");
+
+  //Activate content
+  const content = document.querySelector(
+    `.operations__content--${clickedBtn.dataset.tab}`,
+  );
+  console.log(content);
+  tabsContent.forEach((content) => {
+    content.classList.remove("operations__content--active");
+  });
+  content.classList.add("operations__content--active");
+});
+
+// Implementing Faded effect on hover on nav links:
+const navbarContainer = document.querySelector(".nav");
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector(".nav__logo");
+
+    siblings.forEach((item) => {
+      if (item !== link) {
+        item.style.opacity = this;
+      }
+      logo.style.opacity = this;
+    });
+  }
+};
+
+//Old way
+// navbarContainer.addEventListener("mouseover", function (e) {
+//   handleHover(e, "0.5");
+// });
+// navbarContainer.addEventListener("mouseout", function (e) {
+//   handleHover(e, "1");
+// });
+
+//Better way
+navbarContainer.addEventListener("mouseover", handleHover.bind(0.5));
+navbarContainer.addEventListener("mouseout", handleHover.bind(1));
+
+// // Bad way of Implementing sticky navigation after certain scroll point
+
+// const initialCoords = section1.getBoundingClientRect();
+
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY >= initialCoords.top) {
+//     navbarContainer.classList.add("sticky");
+//   } else {
+//     navbarContainer.classList.remove("sticky");
+//   }
+// });
+
+// IntersectionObserver API
+
+const header = document.querySelector(".header");
+const navHeight = navbarContainer.getBoundingClientRect().height;
+
+const obsCallback = function ([entry], observer) {
+  if (!entry.isIntersecting) {
+    navbarContainer.classList.add("sticky");
+  } else {
+    navbarContainer.classList.remove("sticky");
+  }
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+
+observer.observe(header);
+
+// Implementing lazy loading images using Intersection Observer API
+const targetImages = document.querySelectorAll("img[data-src]");
+
+const loading = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  //replace src attribute with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+const loadingOption = {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+};
+
+const imgObserver = new IntersectionObserver(loading, loadingOption);
+
+targetImages.forEach((img) => {
+  imgObserver.observe(img);
+});
+
+// Revealing section on scroll using Intersection API
+
+const sections = document.querySelectorAll(".section");
+
+const revealCallback = function (entries, observer) {
+  console.log("These are entries", entries);
+  const [entry] = entries;
+  console.log("This is entry", entry);
+  console.log("This is entry target", entry.target);
+
+  //remove hidden class
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+
+  sectionObserver.unobserve(entry.target);
+};
+
+const revealOptions = {
+  root: null,
+  threshold: 0.15,
+};
+
+const sectionObserver = new IntersectionObserver(revealCallback, revealOptions);
+
+sections.forEach((section) => {
+  sectionObserver.observe(section);
 });
